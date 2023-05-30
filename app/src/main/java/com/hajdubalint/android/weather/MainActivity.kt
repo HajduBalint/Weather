8a1725e191944230e39fc8b3ff5a11e59675dded
HajduBalint <hajdu.balint07@gmail.com>
2023-05-30 03:44:24 +0200

    Added analytics to the project

--- app/src/main/java/com/hajdubalint/android/weather/MainActivity.kt
+++ app/src/main/java/com/hajdubalint/android/weather/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.hajdubalint.android.weather.navigation.NavGraph
 import com.hajdubalint.android.weather.navigation.NavigationUtil
 import com.hajdubalint.android.weather.ui.theme.WeatherTheme
@@ -16,8 +19,12 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigationUtil: NavigationUtil
 
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        analytics = Firebase.analytics
         setContent {
             navigationUtil.setController(rememberNavController())
             WeatherTheme {
